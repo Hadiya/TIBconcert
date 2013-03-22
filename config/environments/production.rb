1a7800d977eea10aa71e3ba08daece62a044bfca
DipakPanchal <dipak@complitech.net>
2013-03-22 17:26:45 +0530

    pro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ TibConcert::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -69,7 +69,7 @@ TibConcert::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
    config.assets.debug = true
-   
+
  config.FACEBOOK_APP_ID      = '340501396062129'
 config.facebook_secret    = 'c69cf9f0eb873a1bf7835a654a4b38cf'
 end
