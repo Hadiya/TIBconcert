a063644bacd5bb4c9cf11399c45a12edad5ea262
DipakPanchal <dipak@complitech.net>
2013-03-23 12:39:05 +0530

    faceboo_app id set

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TibConcert::Application.initialize!
-ENV['FACEBOOK_APP_ID']= '340501396062129'
+#ENV['FACEBOOK_APP_ID']= '172721912882206'
