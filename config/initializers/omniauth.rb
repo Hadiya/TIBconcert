4bd0eecd371e69ef711d63fd467611a715839a7e
DipakPanchal <dipak@complitech.net>
2013-03-23 11:58:36 +0530

    path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "340501396062129","c69cf9f0eb873a1bf7835a654a4b38cf",{:scope => 'email,offline_access'}
+  provider :facebook, "172721912882206","479145601c5a34d7f5aba82e062be9fe",{:scope => 'email,offline_access'}
 end
+
