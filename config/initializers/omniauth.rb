9261240b567db6ebd52c5c56bad18c89901d3c6d
DipakPanchal <dipak@complitech.net>
2013-03-23 13:04:41 +0530

    set facebook_app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "503806902990007","62f4fdf3c1e048d1d3a2d44787c085bd",{:scope => 'email,offline_access'}
+  #provider :facebook, "503806902990007","62f4fdf3c1e048d1d3a2d44787c085bd",{:scope => 'email,offline_access'}
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],{:scope => 'email,offline_access'}
 end
 
